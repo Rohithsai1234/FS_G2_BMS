cb3cff17b81ca918389f689ae9f06cb26653cee9
Rohithsai1234 <88870759+Rohithsai1234@users.noreply.github.com>
2022-07-24 16:48:59 +0530

    Add files via upload

---
+++ BookStoreManagementSystem/src/main/java/com/obsm/main/model/Book.java
@@ -0,0 +1,68 @@
+package com.obsm.main.model;
+
+import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "book")
+@JsonIgnoreProperties(value= {"handler","hibernateLazyInitializer","FieldHandler"})
+public class Book {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "author")
+	private String author;
+	
+	@Column(name = "price")
+	private String price;
+
+	@Column(name = "picByte", length = 100000)
+	private byte[] picByte;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public byte[] getPicByte() {
+		return picByte;
+	}
+
+	public void setPicByte(byte[] picByte) {
+		this.picByte = picByte;
+	}
+}
