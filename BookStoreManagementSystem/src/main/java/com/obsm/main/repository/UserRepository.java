6724b0fb18a7a10e336aba76f11586af2845f15e
Rohithsai1234 <88870759+Rohithsai1234@users.noreply.github.com>
2022-07-24 19:07:07 +0530

    Delete BookStoreManagementSystem directory

--- BookStoreManagementSystem/src/main/java/com/obsm/main/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.obsm.main.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.obsm.main.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long>{
-
-}
